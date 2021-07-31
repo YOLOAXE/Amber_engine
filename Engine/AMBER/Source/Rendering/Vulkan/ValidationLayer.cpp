9f86aa705d449a3923d76deb01fd7469fdd84aa9
YOLOAXE <mickael.albarello77@gmail.com>
2021-07-31 19:27:32 +0200

    validation layer

--- Engine/AMBER/Source/Rendering/Vulkan/ValidationLayer.cpp
+++ Engine/AMBER/Source/Rendering/Vulkan/ValidationLayer.cpp
@@ -3,7 +3,7 @@
 namespace Ge
 {
 	ValidationLayer * ValidationLayer::s_pInstance = nullptr;
-    const std::vector<const char*> validationLayers = { "VK_LAYER_LUNARG_standard_validation"/*"VK_LAYER_KHRONOS_validation"*/ };
+    const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
 
 	bool ValidationLayer::initialize(VulkanMisc * vM)
 	{
